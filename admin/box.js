b32fcecee3d88dbaa923b3f57fd84c1cf4a8a5c0
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-24 13:16:34 +0000

    deploy from git
    git-svn-id: https://plugins.svn.wordpress.org/posts-to-posts/trunk@442920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/box.js
+++ admin/box.js
@@ -27,7 +27,7 @@ $('.p2p-box').each(function() {
 	var
 		$metabox = $(this),
 		$connections = $metabox.find('.p2p-connections'),
-		$spinner = $('<img>', {'src': P2PAdmin.spinner});
+		$spinner = $('<img>', {'src': P2PAdmin.spinner, 'class': 'p2p-spinner'});
 
 	function get_base_data() {
 		return {
@@ -209,7 +209,7 @@ $('.p2p-box').each(function() {
 	});
 
 	var searchTab = new PostsTab('.p2p-tab-search');
-	var recentTab = new PostsTab('.p2p-tab-recent');
+	var listTab = new PostsTab('.p2p-tab-list');
 
 	// Search posts
 	var delayed, $searchInput = $metabox.find('.p2p-tab-search :text');
