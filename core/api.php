3cb7c12470597792241a295fa2b6680de55be08b
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-25 22:26:29 +0000

    0.9.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/posts-to-posts/tags/0.9.1@443405 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/api.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * Register a connection between two post types.
+ * This creates the appropriate meta box in the admin edit screen
+ *
+ * @param array $args See https://github.com/scribu/wp-posts-to-posts/wiki/p2p_register_connection_type()
+ * @return bool|object False on failure, P2P_Connection_Type instance on success.
+ */
+function p2p_register_connection_type( $args ) {
+	if ( !did_action('init') ) {
+		trigger_error( "Connection types should not be registered before the 'init' hook.", E_USER_NOTICE );
+	}
+
+	$argv = func_get_args();
+
+	if ( count( $argv ) > 1 ) {
+		$args = array();
+		@list( $args['from'], $args['to'], $args['reciprocal'] ) = $argv;
+	}
+
+	$args = wp_parse_args( $args, array(
+		'show_ui' => true,
+		'fields' => array(),
+		'context' => 'side',
+	) );
+
+	return P2P_Connection_Type::make_instance( $args );
+}
+
+/**
+ * Connect a post to one or more other posts.
+ *
+ * @param int|array $from The first end of the connection
+ * @param int|array $to The second end of the connection
+ * @param array $data additional data about the connection
+ */
+function p2p_connect( $from, $to, $data = array() ) {
+	foreach ( (array) $from as $from ) {
+		foreach ( (array) $to as $to ) {
+			P2P_Storage::connect( $from, $to, $data );
+		}
+	}
+}
+
+/**
+ * Disconnect a post from or more other posts.
+ *
+ * @param int|array $from The first end of the connection
+ * @param int|array|string $to The second end of the connection
+ * @param array $data additional data about the connection to filter against
+ */
+function p2p_disconnect( $from, $to, $data = array() ) {
+	foreach ( (array) $from as $from ) {
+		foreach ( (array) $to as $to ) {
+			P2P_Storage::disconnect( $from, $to, $data );
+		}
+	}
+}
+
+/**
+ * Get a list of connected posts.
+ *
+ * Low-level function. Use new WP_Query( array( 'connected' => $post_id ) ) instead.
+ *
+ * @param int $post_id One end of the connection
+ * @param string $direction The direction of the connection. Can be 'to', 'from' or 'any'
+ * @param array $data additional data about the connection to filter against
+ *
+ * @return array( p2p_id => post_id )
+ */
+function p2p_get_connected( $post_id, $direction = 'any', $data = array() ) {
+	return P2P_Storage::get( $post_id, $direction, $data );
+}
+
+/**
+ * See if a certain post is connected to another one
+ *
+ * @param int $from The first end of the connection
+ * @param int $to The second end of the connection
+ * @param array $data additional data about the connection to filter against
+ *
+ * @return bool True if the connection exists, false otherwise
+ */
+function p2p_is_connected( $from, $to, $data = array() ) {
+	$ids = p2p_get_connected( $from, $to, $data );
+
+	return !empty( $ids );
+}
+
+/**
+ * Delete one or more connections
+ *
+ * @param int|array $p2p_id Connection ids
+ *
+ * @return int Number of connections deleted
+ */
+function p2p_delete_connection( $p2p_id ) {
+	return P2P_Storage::delete( $p2p_id );
+}
+
+/**
+ * List some posts.
+ *
+ * @param object|array A WP_Query instance, or a list of post objects
+ * @param array $args (optional)
+ */
+function p2p_list_posts( $posts, $args = array() ) {
+	if ( is_object( $posts ) )
+		$posts = $posts->posts;
+
+	$args = wp_parse_args( $args, array(
+		'before_list' => '<ul>', 'after_list' => '</ul>',
+		'before_item' => '<li>', 'after_item' => '</li>',
+		'template' => false
+	) );
+
+	extract( $args, EXTR_SKIP );
+
+	if ( empty( $posts ) )
+		return;
+
+	echo $before_list;
+
+	foreach ( $posts as $post ) {
+		$GLOBALS['post'] = $post;
+
+		setup_postdata( $post );
+
+		echo $before_item;
+
+		if ( $template )
+			locate_template( $template, true, false );
+		else
+			echo html( 'a', array( 'href' => get_permalink( $post->ID ) ), get_the_title( $post->ID ) );
+
+		echo $after_item;
+	}
+
+	echo $after_list;
+
+	wp_reset_postdata();
+}
+
---
+++ core/type.php
@@ -0,0 +1,304 @@
+<?php
+
+class P2P_Connection_Type {
+
+	static $instances = array();
+
+	protected $args;
+
+	protected function __construct( $args ) {
+		$this->args = $args;
+
+	}
+
+	public function __get( $key ) {
+		return $this->args[$key];
+	}
+
+	public function get_instance( $hash ) {
+		if ( isset( self::$instances[ $hash ] ) )
+			return self::$instances[ $hash ];
+
+		return false;
+	}
+
+	public function make_instance( $args ) {
+		$args = wp_parse_args( $args, array(
+			'from' => '',
+			'to' => '',
+			'data' => array(),
+			'reciprocal' => false,
+			'sortable' => false,
+			'prevent_duplicates' => true,
+			'title' => '',
+		) );
+
+		foreach ( array( 'from', 'to' ) as $key ) {
+			$args[ $key ] = array_values( array_filter( (array) $args[ $key ], 'post_type_exists' ) );
+			if ( empty( $args[ $key ] ) ) {
+				trigger_error( "'$key' arg doesn't contain any valid post types", E_USER_WARNING );
+				return false;
+			}
+			sort( $args[ $key ] );
+		}
+
+		$common = array_intersect( $args['from'], $args['to'] );
+		if ( !empty( $common ) && count( $args['from'] ) + count( $args['to'] ) > 2 ) {
+			trigger_error(
+				"Some post types appear both in 'to' and 'from' arguments: " . implode( ' ', $common ),
+				E_USER_WARNING );
+			return false;
+		}
+
+		$error = false;
+
+		foreach ( array( 'from', 'to' ) as $key ) {
+			foreach ( $args[$key] as $ptype ) {
+				if ( !post_type_exists( $ptype ) ) {
+					trigger_error( "The '$ptype' post type does not exist.", E_USER_WARNING );
+					$error = true;
+				}
+			}
+		}
+
+		if ( $error )
+			return false;
+
+		$hash = md5( serialize( wp_array_slice_assoc( $args, array( 'from', 'to', 'data' ) ) ) );
+
+		if ( isset( self::$instances[ $hash ] ) ) {
+			trigger_error( 'Connection type is already defined.', E_USER_NOTICE );
+			return self::$instances[ $hash ];
+		}
+
+		return self::$instances[ $hash ] = new P2P_Connection_Type( $args );
+	}
+
+	/**
+	 * Get connection direction.
+	 *
+	 * @param int|string $arg A post id or a post type.
+	 * @param bool $respect_reciprocal Whether to take the 'reciprocal' arg into consideration.
+	 *
+	 * @return bool|string False on failure, 'any', 'to' or 'from' on success.
+	 */
+	public function get_direction( $arg, $respect_reciprocal = false ) {
+		if ( $post_id = (int) $arg ) {
+			$post = get_post( $post_id );
+			if ( !$post )
+				return false;
+			$post_type = $post->post_type;
+		} else {
+			$post_type = $arg;
+		}
+
+		if ( $post_type == $this->to[0] && $this->from[0] == $post_type )
+			$direction = 'any';
+		elseif ( in_array( $post_type, $this->to ) )
+			$direction = 'to';
+		elseif ( in_array( $post_type, $this->from ) )
+			$direction = 'from';
+		else
+			return false;
+
+		if ( $respect_reciprocal && !$this->reciprocal && 'to' == $direction )
+			return false;
+
+		return $direction;
+	}
+
+	public function get_other_post_type( $direction ) {
+		return 'from' == $direction ? $this->to : $this->from;
+	}
+
+	public function can_create_post( $direction ) {
+		$ptype = $this->get_other_post_type( $direction );
+
+		if ( count( $ptype ) > 1 )
+			return false;
+
+		return current_user_can( get_post_type_object( $ptype[0] )->cap->edit_posts );
+	}
+
+	public function get_title( $direction ) {
+		$title = $this->args['title'];
+
+		if ( is_array( $title ) ) {
+			$key = ( 'to' == $direction ) ? 'to' : 'from';
+
+			if ( isset( $title[ $key ] ) )
+				$title = $title[ $key ];
+			else
+				$title = '';
+		}
+
+		return $title;
+	}
+
+	private function get_base_args( $direction, $extra_qv ) {
+		return array_merge( $extra_qv, array(
+			'post_type' => $this->get_other_post_type( $direction ),
+			'suppress_filters' => false,
+			'ignore_sticky_posts' => true
+		) );
+	}
+
+	/**
+	 * Get a list of posts that are connected to a given post.
+	 *
+	 * @param int $post_id A post id.
+	 * @param array $extra_qv Additional query variables to use.
+	 *
+	 * @return bool|object False on failure; A WP_Query instance on success.
+	 */
+	public function get_connected( $post_id, $extra_qv = array(), $_direction = false ) {
+		$direction = $_direction ? $_direction : $this->get_direction( $post_id );
+		if ( !$direction )
+			return false;
+
+		$args = $this->get_base_args( $direction, $extra_qv );
+
+		_p2p_append( $args, array(
+			'connected' => $post_id,
+			'connected_direction' => $direction,
+			'connected_meta' => $this->data,
+		) );
+
+		if ( $this->sortable && 'from' == $direction ) {
+			_p2p_append( $args, array(
+				'connected_orderby' => $this->sortable,
+				'connected_order' => 'ASC',
+				'connected_order_num' => true,
+			) );
+		}
+
+		$args = apply_filters( 'p2p_connected_args', $args, $this );
+
+		return new WP_Query( $args );
+	}
+
+	/**
+	 * Get a list of posts that could be connected to a given post.
+	 *
+	 * @param int $post_id A post id.
+	 * @param array $extra_qv Additional query variables to use.
+	 *
+	 * @return bool|object False on failure; A WP_Query instance on success.
+	 */
+	public function get_connectable( $post_id, $extra_qv, $_direction = false ) {
+		$direction = $_direction ? $_direction : $this->get_direction( $post_id );
+		if ( !$direction )
+			return false;
+
+		$args = $this->get_base_args( $direction, $extra_qv );
+
+		if ( $this->prevent_duplicates ) {
+			if ( !isset( $args['post__not_in'] ) ) {
+				$args['post__not_in'] = array();
+			}
+
+			_p2p_append( $args['post__not_in'], P2P_Storage::get( $post_id, $direction, $this->data ) );
+		}
+
+		$args = apply_filters( 'p2p_connectable_args', $args, $this );
+
+		return new WP_Query( $args );
+	}
+
+	/**
+	 * Optimized inner query, after the outer query was executed.
+	 *
+	 * Populates each of the outer querie's $post objects with a 'connected' property, containing a list of connected posts
+	 *
+	 * @param object $query WP_Query instance.
+	 * @param string|array $extra_qv Additional query vars for the inner query.
+	 */
+	public function each_connected( $query, $extra_qv = array() ) {
+		if ( empty( $query->posts ) || !is_object( $query->posts[0] ) )
+			return;
+
+		$post_type = $query->get( 'post_type' );
+		if ( empty( $post_type ) )
+			$post_type = 'post';
+
+		$direction = $this->get_direction( $post_type );
+		if ( !$direction )
+			return;
+
+		$prop_name = 'connected';
+
+		$posts = array();
+
+		foreach ( $query->posts as $post ) {
+			$post->$prop_name = array();
+			$posts[ $post->ID ] = $post;
+		}
+
+		// ignore pagination
+		foreach ( array( 'showposts', 'posts_per_page', 'posts_per_archive_page' ) as $disabled_qv ) {
+			if ( isset( $extra_qv[ $disabled_qv ] ) ) {
+				trigger_error( "Can't use '$disabled_qv' in an inner query", E_USER_WARNING );
+			}
+		}
+		$extra_qv['nopaging'] = true;
+
+		$q = $this->get_connected( array_keys( $posts ), $extra_qv, $direction );
+
+		foreach ( $q->posts as $inner_post ) {
+			if ( $inner_post->ID == $inner_post->p2p_from )
+				$outer_post_id = $inner_post->p2p_to;
+			elseif ( $inner_post->ID == $inner_post->p2p_to )
+				$outer_post_id = $inner_post->p2p_from;
+			else {
+				trigger_error( "Corrupted data for post $inner_post->ID", E_USER_WARNING );
+				continue;
+			}
+
+			if ( $outer_post_id == $inner_post->ID ) {
+				trigger_error( 'Post connected to itself.', E_USER_WARNING );
+				continue;
+			}
+
+			array_push( $posts[ $outer_post_id ]->$prop_name, $inner_post );
+		}
+	}
+
+	public function connect( $from, $to ) {
+		$post_from = get_post( $from );
+		$post_to = get_post( $to );
+
+		if ( !$post_from || !$post_to ) {
+			return false;
+		}
+
+		$args = array( $from, $to );
+
+		if ( $post_from->post_type == $this->to )
+			$args = array_reverse( $args );
+
+		$p2p_id = false;
+
+		if ( $this->prevent_duplicates ) {
+			$p2p_ids = P2P_Storage::get( $args[0], $args[1], $this->data );
+
+			if ( !empty( $p2p_ids ) )
+				$p2p_id = $p2p_ids[0];
+		}
+
+		if ( !$p2p_id ) {
+			$p2p_id = P2P_Storage::connect( $args[0], $args[1], $this->data );
+		}
+
+		return $p2p_id;
+	}
+
+	public function disconnect( $post_id, $_direction = false ) {
+		$direction = $_direction ? $_direction : $this->get_direction( $post_id );
+		P2P_Storage::disconnect( $post_id, $direction, $this->data );
+	}
+
+	public function delete_connection( $p2p_id ) {
+		return P2P_Storage::delete( $p2p_id );
+	}
+}
+
